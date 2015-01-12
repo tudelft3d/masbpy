946113fd0598048e2565b7e918d80a31a3b43718
Ylannl <r.y.peters@tudelft.nl>
2015-01-12 14:00:50 +0100

    example file for multiprocessing

---
+++ example_mp.py
@@ -0,0 +1,14 @@
+from masbpy.ma_fast import MASB
+from masbpy import io_ply
+from masbpy.write import write_coords_filtered, write_coords_lfs_colored, write_ma_coords
+
+
+if __name__ == '__main__':
+    # read points and normals from a ply file
+    datadict = io_ply.read_ply('example-data/house_dyke_tree.ply')
+    
+    # compute interior and exterior MAT
+    ma = MASB(datadict, 10)
+    ma.compute_mp()
+    # write MA points to file
+    write_ma_coords(datadict)
