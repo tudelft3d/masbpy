074a168933753e695c0fc8b34f742978faaf64c7
Ylannl <r.y.peters@tudelft.nl>
2015-01-19 13:39:40 +0100

    add simple las reader based on laspy

---
+++ masbpy/io_las.py
@@ -0,0 +1,27 @@
+# This file is part of masbpy.
+
+# masbpy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# masbpy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with masbpy.  If not, see <http://www.gnu.org/licenses/>.
+
+# Copyright 2014 Ravi Peters
+
+import numpy as np
+
+def read_las(infile, keys):
+	from laspy.file import File
+
+	inFile = File(infile)
+	datadict = {}
+	datadict['coords'] = np.column_stack([ np.array(a, dtype=np.float32) for a in [inFile.x, inFile.y, inFile.z] ])
+	return datadict
+
