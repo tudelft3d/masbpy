78418abafa895429c1d2b42d31800a92479b58a1
Ylannl <r.y.peters@tudelft.nl>
2015-02-25 09:54:16 +0100

    allow metacompute to work w/o numba

--- masbpy/metacompute.py
+++ masbpy/metacompute.py
@@ -1,5 +1,26 @@
+# This file is part of masbpy.
+
+# masbpy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# masbpy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with masbpy.  If not, see <http://www.gnu.org/licenses/>.
+
+# Copyright 2015 Ravi Peters
+
 import numpy as np
-from algebra_numba import norm, dot, equal, proj, compute_radius, cos_angle
+try: 
+    import numba
+    from algebra_numba import norm, dot, equal, compute_radius, cos_angle
+except:
+    from algebra import norm, dot, equal, compute_radius, cos_angle
 
 
 def compute_lfs(datadict, k=10):
